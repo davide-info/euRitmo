03f880c908a4a531f47e71d7cd6b72e458f4bb33
davide-info <davideinfo4@gmail.com>
2022-09-02 14:39:13 +0200

    Aggiungi README.md.

---
+++ euRitmo/Form1.vb
@@ -0,0 +1,56 @@
+﻿Imports System.Text
+Imports System.IO
+Imports System.Text.RegularExpressions
+
+Public Class Form1
+    Private Function testParseFile(fileName As String)
+        Dim result As New StringBuilder()
+        Dim lines = File.ReadAllLines(fileName)
+
+
+        For Each line As String In lines
+            result.Append(parseLine(line))
+
+        Next
+        MessageBox.Show("RISULTATO " & result.ToString())
+
+        Return result.ToString()
+
+
+    End Function
+
+    Private Function parseLine(line As String) As String
+        Dim result As New StringBuilder()
+        Dim regexStr = "(?<tes>TES)(?<val1>[a-z-A-Z-0-9]{2,40})"
+        Dim regex As New Regex(regexStr)
+        ''Console.WriteLine(regex)
+        Dim names As String() = regex.GetGroupNames()
+        Dim match As Match = regex.Match(line)
+        Dim str As String
+        For Each name As String In names
+            If match.Success Then
+                str = "GRUPPO CATTURATO " & name & " " & match.Groups(name).Value & Environment.NewLine
+                result.Append(str)
+
+
+            End If
+        Next
+
+        Return result.ToString()
+
+
+    End Function
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+
+        If OpenFileDialog1.ShowDialog = DialogResult.OK Then
+            Dim str = testParseFile(OpenFileDialog1.FileName)
+            ''TextBox1.AppendText(str)
+            ''MessageBox.Show(str)
+
+
+        End If
+
+    End Sub
+End Class
---
+++ euRitmo/Tg904.vb
@@ -0,0 +1,88 @@
+﻿Public Class Tg904
+    Private Tes As Tes
+
+
+    Friend Sub setTes(Tes As Tes)
+        Me.Tes = Tes
+    End Sub
+
+
+
+
+End Class
+
+Public Class Dtope
+    Private DTOPE_SAA As String()
+    Private DTOPE_MM As String()
+    Private DTOPE_GG As String()
+    Public Sub setDtopeMM(DTOPE_MM() As String)
+        Me.DTOPE_MM = DTOPE_MM
+
+
+
+    End Sub
+    Public Function getDtopeMM() As String()
+        Return Me.DTOPE_MM
+
+
+    End Function
+
+    Public Sub setDtopseSaa(DTOPE_SAA As String())
+        Me.DTOPE_SAA = DTOPE_SAA
+
+
+    End Sub
+    Public Function GetDtopeSaa() As String()
+        Return Me.DTOPE_SAA
+
+    End Function
+
+
+End Class
+Public Class Tes
+    Private TG904_TRK As String
+    Private TG904_OP As String
+    Private TG904_TMOPE As String
+    Private TG904_DTPC As String
+    Private TG904_CMPT As String
+    Private TG904_STATO_ORD As String()
+    Private TG904_COD_BUYER As String()
+    Private TG904_DES_BUYER As String()
+    Private TG904_NART_ORD_T As String()
+    Private TG904_NART_CON_T As String()
+    Private TG904_CART_ORD_OMT_T As String()
+    Private TG904_CART_CON_T As String()
+    Private TG904_CART_CON_OM_T As String()
+    Private TG904_PLT_ORD_T As String()
+    Private TG904_PLT_CON_T As String()
+    Private TG904_VAL_ORD_T As String()
+    Private TG904_NOTE_ORD_T As String()
+    Private TG904_SC_ORD_T As String()
+    Private TG90_DTCON As String()
+    Private TG904_DTDOC As String()
+
+
+    Sub New()
+
+    End Sub
+
+    Sub New(ByVal stringTes As String)
+        TG904_TRK = stringTes.Substring(0, 3)
+        TG904_OP = stringTes.Substring(3, 1)
+        TG904_TMOPE = stringTes.Substring(12, 8)
+
+
+
+    End Sub
+
+
+
+
+
+
+
+
+
+
+
+End Class
