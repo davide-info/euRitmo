d03c639c3d3074ab1b01de981a46557f63f3ba5a
davide-info <davideinfo4@gmail.com>
2022-09-09 17:39:38 +0200

    Modificati quest files

--- euRitmo/ordine_edi.vb
+++ euRitmo/ordine_edi.vb
@@ -13,7 +13,7 @@
     Friend ftx As Ftx
     Friend pat As Pat
     Friend tod As Tod
-    Friend lin As lin
+    Friend lin As New List(Of lin)
     Friend mea As MEA
     Friend pac As Pac
     Friend mei As Mei
@@ -26,28 +26,32 @@
 End Class
 
 Public Class Bgm
-    Friend ID_EDI_MITT As String
-    Friend ID_EDI_MITT1 As String
-    Friend ID_EDI_MITT2 As String
-    Friend ID_EDI_MITT3 As String
-    Friend IED_EDI_DEST As String
-    Friend ID_EDI_DEST1 As String
-    Friend ID_EDI_DEST2 As String
-    Friend ID_EDI_DEST3 As String
-    Friend TIPODOC As String
-    Friend NUMDOC As String
-    Friend FORDPROM As String
-    Friend NUMORDF As String
-    Friend DATAORDF As Integer
-    Friend ORAORDF As Integer
-    Friend NUMORDC As String
-    Friend DATAORDC As Integer
-    Friend ORAORDC As Integer
-    Friend NUMPROFDF As Integer
-    Friend numpordc As Integer
-    Friend Codazion As String
-    Friend FLAGIMPE As String
-    Friend TIPOORD As String
+
+    Friend TIPOREC As String = "BGM"
+    Friend ID_EDI_MITT1 As String = "".PadLeft(35)
+    Friend ID_EDI_MITT2 As String = "".PadLeft(4)
+    Friend ID_EDI_MITT3 As String = "".PadLeft(14)
+    Friend ID_EDI_DEST1 As String = "".PadLeft(35)
+    Friend ID_EDI_DEST2 As String = "".PadLeft(4)
+    Friend ID_EDI_DEST3 As String = "".PadLeft(14)
+    Friend TIPODOC As String = "".PadLeft(6)
+    Friend NUMDOC As String = "".PadLeft(35)
+    Friend DATADOC As String = "".PadLeft(8)
+    Friend FORDPROM As String = "".PadLeft(4)
+    Friend NUMORDF As String = "".PadLeft(14)
+    Friend DATAORDF As String
+    Friend ORAORDF As String ''"".PadLeft(4)
+    Friend NUMORDC As String = "".PadLeft(14)
+    Friend DATAORDC As String
+    Friend ORAORDC As String
+    Friend NUMPROFDF As String
+    Friend numpordc As String
+    Friend Codazion As String = "".PadLeft(3)
+    Friend FLAGIMPE As String = "".PadLeft(3)
+    Friend TIPOORD As String = "".PadLeft(3)
+    Public Sub New()
+
+    End Sub
 End Class
 
 'Public Enum TipoDoc
@@ -66,78 +70,83 @@ End Class
 'End Enum
 
 Public Class RFF
-    Friend TIPOREC As String
-    Friend FORDPROM As String
-    Friend NUMORDF As String
-    Friend DATAORDF As Integer
-    Friend ORAORDF As Integer
-    Friend NUMORDC As String
-    Friend DATAORDC As Integer
-    Friend ORAORDC As Integer
+    Friend TIPOREC As String = "RFF"
+    Friend FORDPROM As String = "".PadLeft(3)
+    Friend NUMORDF As String = "".PadLeft(35)
+    Friend DATAORDF As String
+    Friend ORAORDF As String
+    Friend NUMORDC As String = "".PadLeft(35)
+    Friend DATAORDC As String
+    Friend ORAORDC As String
 
-    Friend NUMPORDF As String
-    Friend NUMPORDC As String
+    Friend NUMPORDF As String = "".PadLeft(35)
+    Friend NUMPORDC As String = "".PadLeft(35)
+
 End Class
 Public Class RFC
-    Friend TIPOREC As String
-    Friend NUMCO As String
-    Friend DATAINCO As Integer
-    Friend DATAFICO As Integer
+    Friend TIPOREC As String = "RFC"
+    Friend NUMCO As String = "".PadLeft(35)
+    Friend DATAINCO As String
+    Friend DATAFICO As String
 
 End Class
 Public Class NAS
-    Friend TIPOREC As String
-    Friend CODFORN As String
-    Friend QCODFORN As String
-    Friend RAGSOCF As String
-    Friend INDIRF As String
-    Friend CITTAF As String
-    Friend PROVF As String
-    Friend CAPF As String
-    Friend NZAIOF As String
+    Friend TIPOREC As String = "NAS"
+    Friend CODFORN As String = "".PadLeft(17)
+    Friend QCODFORN As String = "".PadLeft(3)
+    Friend RAGSOCF As String = "".PadLeft(70)
+    Friend INDIRF As String = "".PadLeft(70)
+    Friend CITTAF As String = "".PadLeft(35)
+    Friend PROVF As String = "".PadLeft(9)
+    Friend CAPF As String = "".PadLeft(9)
+    Friend NAZIOF As String = "".PadLeft(3)
 
 End Class
 
 Public Class CTA
-    Friend TIPOREC As String
-    Friend FUNZCONT As String
-    Friend TELEFONO As String
-    Friend FAX As String
-    Friend TELEX As String
-    Friend EMAIL As String
+    Friend TIPOREC As String = "CTA"
+    Friend FUNZCONT As String = "".PadLeft(3)
+    Friend TELEFONO As String = "".PadLeft(25)
+    Friend FAX As String = "".PadLeft(25)
+    Friend TELEX As String = "".PadLeft(25)
+    Friend EMAIL As String = "".PadLeft(70)
 
 End Class
 Public Class NAB
-    Friend tiporec As String
-    Friend QCODBY As String
-    Friend RAGCOSB As String
-    Friend CITTAB As String
-    Friend CAPB As String
-    Friend NAZIOB As String
+    Friend TIPOREC As String = "NAB"
+
+    Friend CODBUYER As String = "".PadLeft(17)
+    Friend QCODBUY As String = "".PadLeft(3)
+    Friend REGSOC As String = "".PadLeft(70)
+    Friend INDRB As String = "".PadLeft(70)
+    Friend CITTAB As String = "".PadLeft(35)
+    Friend CAPB As String = "".PadLeft(9)
+    Friend PROvB As String = "".PadLeft(9)
+    Friend NAZIOB As String = "".PadLeft(3)
 End Class
 
 Public Class Nad
-    Friend TIPOREC As String
-    Friend CODCONS As String
-    Friend QCODCONS As String
-    Friend RAGSOCD As String
-    Friend INDIRD As String
-    Friend CITTAD As String
-    Friend PROVD As String
-    Friend CAPD As String
-    Friend NAZIOD As String
+    Friend TIPOREC As String = "NAD"
+    Friend CODCONS As String = "".PadLeft(17)
+    Friend QCODCONS As String = "".PadLeft(3)
+    Friend RAGSOCD As String = "".PadLeft(70)
+    Friend INDIRD As String = "".PadLeft(70)
+    Friend CITTAD As String = "".PadLeft(35)
+    Friend PROVD As String = "".PadLeft(9)
+    Friend CAPD As String = "".PadLeft(9)
+    Friend NAZIOD As String = "".PadLeft(3)
 End Class
 
 Public Class Nai
-    Friend TIPOREC As String
-    Friend CODFATT As String
-    Friend QCODFATT As String
-    Friend RAGSOCI As String
-    Friend INDIRi As String
-    Friend CITTAI As String
-    Friend PROVI As String
-    Friend CAPI As String
-    Friend NAZOI As String
+    Friend TIPOREC As String = "NAI"
+    Friend CODFATT As String = "".PadLeft(17)
+    Friend QCODFATT As String = "".PadLeft(3)
+    Friend RAGSOCI As String = "".PadLeft(70)
+    Friend INDIRi As String = "".PadLeft(70)
+    Friend CITTAI As String = "".PadLeft(35)
+    Friend PROVI As String = "".PadLeft(9)
+    Friend CAPI As String = "".PadLeft(9)
+    Friend NAZOI As String = "".PadLeft(3)
 
 End Class
 
@@ -155,15 +164,15 @@ End Class
 'End Enum
 
 Public Class Nac
-    Friend TIPOREC As String
-    Friend CODMAGI As String
-    Friend QCODMAGI As String
-    Friend RAGSOCC As String
-    Friend INDIRC As String
-    Friend CITTAC As String
-    Friend PROVC As String
-    Friend CAPC As String
-    Friend NAZIOC As String
+    Friend TIPOREC As String = "NAC"
+    Friend CODMAGI As String = "".PadLeft(17)
+    Friend QCODMAGI As String = "".PadLeft(3)
+    Friend RAGSOCC As String = "".PadLeft(70)
+    Friend INDIRC As String = "".PadLeft(70)
+    Friend CITTAC As String = "".PadLeft(35)
+    Friend PROVC As String = "".PadLeft(9)
+    Friend CAPC As String = "".PadLeft(9)
+    Friend NAZIOC As String = "".PadLeft(3)
 End Class
 
 'Public Enum TipoCodiceMerce
@@ -172,13 +181,15 @@ End Class
 'End Enum
 
 Public Class Nam
-    Friend TIPOREC As String
-    Friend CODMAGP As String
-    Friend QCODMAGP As String
-    Friend CITTAM As String
-    Friend PROVM As String
-    Friend CAPM As String
-    Friend NAZIOM As String
+    Friend TIPOREC As String = "NAM"
+    Friend CODMAGP As String = "".PadLeft(17)
+    Friend QCODMAGP As String = "".PadLeft(3)
+    Friend RAGOSCM As String = "".PadLeft(70)
+    Friend INDRIM As String = "".PadLeft(70)
+    Friend CITTAM As String = "".PadLeft(35)
+    Friend PROVM As String = "".PadLeft(9)
+    Friend CAPM As String = "".PadLeft(9)
+    Friend NAZIOM As String = "".PadLeft(3)
     Public Sub New()
 
 
@@ -198,13 +209,14 @@ End Class
 'End Enum
 
 Public Class Dtm
-    Friend TIPOREC As String
-    Friend DATACONS As Integer
-    Friend ORACONS As Integer
-    Friend TIPODATAc As String
-    Friend DATACON2 As Integer
-    Friend ORACON2 As Integer
-    Friend TIPODATA2 As String
+    Friend TIPOREC As String = "DTM"
+
+    Friend DATACONS As String
+    Friend ORACONS As String
+    Friend TIPODATAc As String = "".PadLeft(3)
+    Friend DATACON2 As String
+    Friend ORACON2 As String
+    Friend TIPODATA2 As String = "".PadLeft(3)
 
 End Class
 
@@ -217,26 +229,27 @@ End Class
 'End Enum
 
 Public Class Ftx
-    Friend TIPOREC As String
-    Friend DIVISA As String
-    Friend NOTE As String
+    Friend TIPOREC As String = "FTX"
+    Friend DIVISA As String = "".PadLeft(3)
+    Friend NOTE As String = "".PadLeft(210)
 End Class
 Public Class Pat
-    Friend TIPOREC As String
-    Friend TIPOCOND As String
-    Friend RIFERMP As String
-    Friend RELTERMP As String
-    Friend UNTEMP As String
-    Friend NUMTEMP As Integer
-    Friend IMPORTO As String
-    Friend DIVISA As String
-    Friend PERC As String
-    Friend DESCRIZ As String
-    Friend BANCACOD As String
-    Friend BANCADESC As String
-    Friend FACTOR As String
-    Friend COPAG As String
-    Friend MEZZOPAG As String
+    Friend TIPOREC As String = "PAT"
+    Friend TIPOCOND As String = "".PadLeft(3)
+    Friend DATASCAD As String
+    Friend RIFERMP As String = "".PadLeft(3)
+    Friend RELTERMP As String = "".PadLeft(3)
+    Friend UNTEMP As String = "".PadLeft(3)
+    Friend NUMTEMP As String
+    Friend IMPORTO As String = "".PadLeft(32)
+    Friend DIVISA As String = "".PadLeft(3)
+    Friend PERC As String = "".PadLeft(14)
+    Friend DESCRIZ As String = "".PadLeft(35)
+    Friend BANCACOD As String = "".PadLeft(35)
+    Friend BANCADESC As String = "".PadLeft(35)
+    Friend FACTOR As String = "".PadLeft(35)
+    Friend COPAG As String = "".PadLeft(3)
+    Friend MEZZOPAG As String = "".PadLeft(3)
 End Class
 
 
@@ -252,125 +265,126 @@ End Class
 'End Enum
 
 Public Class Tod
-    Friend TIPOREC As String
-    Friend CODCONS As String
-    Friend CODCONST As String
-    Friend CODCOND As String
-    Friend DESCOND1 As String
-    Friend DESCOND2 As String
+    Friend TIPOREC As String = "TOD"
+    Friend CODCONS As String = "".PadLeft(3)
+    Friend CODCOST As String = "".PadLeft(3)
+    Friend CODCOND As String = "".PadLeft(3)
+    Friend DESCOND1 As String = "".PadLeft(70)
+    Friend DESCOND2 As String = "".PadLeft(70)
 
 End Class
 Public Class lin
-    Friend TIPOREC As String
-    Friend NUMRIGA As Integer
-    Friend CODICEANCU As String
-    Friend TIPOCODCU As String
-    Friend CODEANTU As String
-    Friend CODFORTU As String
-    Friend CODDISTU As String
-    Friend DESART As String
-    Friend FLINPROM As String
-    Friend QTAORD As String
-    Friend UDMQTAORD As String
-    Friend PRZUNI As String
-    Friend TIPOPRZ As String
-    Friend UDMPRZUN As String
-    Friend NRCUINTU As String
-    Friend CODAZIOL As String
-    Friend QTACONF As String
-    Friend UDMQCONF As String
-    Friend PRZUN2 As String
-    Friend TIPOPRZ2 As String
-    Friend UDMPRUNZ2 As String
-    Friend PRZBASE As String
+    Friend TIPOREC As String = "LIN"
+    Friend NUMRIGA As String
+    Friend CODICEANCU As String = "".PadLeft(35)
+    Friend TIPOCODCU As String = "".PadLeft(3)
+    Friend CODEANTU As String = "".PadLeft(35)
+    Friend CODFORTU As String = "".PadLeft(35)
+    Friend CODDISTU As String = "".PadLeft(35)
+    Friend DESART As String = "".PadLeft(35)
+    Friend FLINPROM As String = "".PadLeft(3)
+    Friend QTAORD As String = ""
+    Friend UDMQTAORD As String = "".PadLeft(3)
+    Friend PRZUNI As String = ""
+    Friend TIPOPRZ As String = "".PadLeft(3)
+    Friend UDMPRZUN As String = "".PadLeft(3)
+    Friend NRCUINTU As String = ""
+    Friend QTACONF As String = ""
+    Friend UDMQCONF As String = "".PadLeft(3)
+    Friend PRZUN2 As String = ""
+    Friend TIPOPRZ2 As String = "".PadLeft(3)
+    Friend UDMPRUNZ2 As String = "".PadLeft(3)
+    Friend PRZBASE As String = ""
 
 End Class
 Public Class MEA
-    Friend TIPOREC As String
-    Friend QUALMISURA As String
-    Friend IDDICOMMNCOD As String
-    Friend SIGNIMISCOD As String
-    Friend QUALUNIMIS As String
-    Friend VALOMISURA As String
-    Friend RANGEMIN As String
-    Friend RANGEMAS As String
+    Friend TIPOREC As String = "MEA"
+    Friend QUALMISURA As String = "".PadLeft(3)
+    Friend IDDICOMMNCOD As String = "".PadLeft(3)
+    Friend SIGNIMISCOD As String = "".PadLeft(3)
+    Friend QUALUNIMIS As String = "".PadLeft(3)
+    Friend VALOMISURA As String = ""
+    Friend RANGEMIN As String = ""
+    Friend RANGEMAS As String = ""
 
 End Class
 Public Class Pac
-    Friend TIPOREC As String
-    Friend NUMRIGA As Integer
-    Friend CODART As String
-    Friend TIPCODART As String
-    Friend UDMQUIMB As String
-    Friend INFIBACOD As String
-    Friend CONIMBCOD As String
-    Friend IDETIPIMB As String
-    Friend RESPAGIMBREST As String
+    Friend TIPOREC As String = "PAC"
+    Friend NUMRIGA As String
+    Friend CODART As String = "".PadLeft(35)
+    Friend TIPCODART As String = "".PadLeft(3)
+    Friend UDMQUIMB As String = "".PadLeft(3)
+    Friend INFIBACOD As String = "".PadLeft(3)
+    Friend CONIMBCOD As String = "".PadLeft(3)
+    Friend IDETIPIMB As String = "".PadLeft(3)
+    Friend RESPAGIMBREST As String = "".PadLeft(3)
 End Class
 Public Class Mei
-    Friend TIPOREC As String
-    Friend QUALMISURA As String
-    Friend IDDEMENCOD As String
-    Friend SIGNIMISCOD As String
-    Friend QUALUNIMIS As String
-    Friend VALOMIMISURA As String
-    Friend RANGEMIN As String
-    Friend RANGEMAS As String
+    Friend TIPOREC As String = "MEI"
+    Friend QUALMISURA As String = "".PadLeft(3)
+    Friend IDDIEMENCOD As String = "".PadLeft(3)
+    Friend SIGNIMISCOD As String = "".PadLeft(3)
+    Friend QUALUNIMIS As String = "".PadLeft(3)
+    Friend VALOMIMISURA As String = ""
+    Friend RANGEMIN As String = ""
+    Friend RANGEMAS As String = ""
 End Class
 Public Class Dtr
-    Friend TIPOREC As String
-    Friend DATRCONS As Integer
-    Friend ORARCONS As Integer
-    Friend TIPODATRC As String
-    Friend DATRCON2 As Integer
-    Friend ORARCON2 As Integer
-    Friend TIPODATTR2 As String
-    Friend QTACONS As String
+    Friend TIPOREC As String = "DTR"
+    Friend DATRCONS As String
+    Friend ORARCONS As String
+    Friend TIPODATRC As String = "".PadLeft(3)
+    Friend DATRCON2 As String
+    Friend ORARCON2 As String
+    Friend TIPODATTR2 As String = "".PadLeft(3)
+    Friend QTACONS As String = ""
 
 End Class
 Public Class Ald
-    Friend TIPOREC As String
-    Friend INSCADD As String
-    Friend DESCR As String
-    Friend INDSEQ As Integer
-    Friend TIPOSCADD As String
-    Friend IMPORTO As String
-    Friend PERC As String
-    Friend FLGPRZUN As String
+    Friend TIPOREC As String = "ALD"
+    Friend INSCADD As String = "".PadLeft(3)
+    Friend DESCR As String = "".PadLeft(35)
+    Friend INDSEQ As String
+    Friend TIPOSCADD As String = "".PadLeft(6)
+    Friend IMPORTO As String = ""
+    Friend PERC As String = ""
+    Friend FLGPRZUN As String = "".PadLeft(3)
 End Class
 Public Class Ftl
-    Friend TIPOREC As String
-    Friend NOTE As String
+    Friend TIPOREC As String = "FTL"
+    Friend NOTE As String = "".PadLeft(140)
 End Class
 
 Public Class Loc
-    Friend TIPOREC As String
-    Friend CODCONS As String
-    Friend QCODCONS As String
-    Friend RAGSOCD As String
-    Friend INDIRD As String
-    Friend CITTAD As String
-    Friend PROVD As String
-    Friend CAPD As String
-    Friend NAZIOD As String
-    Friend QTASPLIt As String
-    Friend UDMQORD As String
+    Friend TIPOREC As String = "LOC"
+    Friend CODCONS As String = "".PadLeft(17)
+    Friend QCODCONS As String = "".PadLeft(3)
+    Friend RAGSOCD As String = "".PadLeft(70)
+    Friend INDIRD As String = "".PadLeft(70)
+    Friend CITTAD As String = "".PadLeft(35)
+    Friend PROVD As String = "".PadLeft(9)
+    Friend CAPD As String = "".PadLeft(9)
+    Friend NAZIOD As String = ""
+    Friend QTASPLIt As String = ""
+    Friend UDMQORD As String = ""
 End Class
 Public Class Dtl
-    Friend TIPOREC As String
-    Friend DATRCONS As Integer?
-    Friend ORARCONS As Integer?
+    Friend TIPOREC As String = "DTL"
+    Friend DATRCONS As String
+    Friend ORARCONS As String
+
+
     Friend TIPODATARC As String
-    Friend DATRCON2 As Integer?
-    Friend ORARCON2 As Integer?
-    Friend TIPODATATR2 As Integer?
-    Friend QTACONS As Integer?
+    Friend DATRCON2 As String
+    Friend ORARCON2 As String
+    Friend TIPODATATR2 As String
+    Friend QTACONS As String
 
 
 End Class
 Public Class Cnt
-    Friend TIPOREC As String
-    Friend QTAORDT As Integer?
+    Friend TIPOREC As String = "CNT"
+    Friend QTAORDT As String
     Friend UDMQORDT As String
     Friend NUMLIT As String
 End Class
